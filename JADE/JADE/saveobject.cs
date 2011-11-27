858c2276cc3e2ea6ef24f19652d6dc1a2cd06f2c
robert.studtrucker@gmail.com <robert.studtrucker@gmail.com@43a69330-ffb3-4c6b-9fb1-d670dd4760cb>
2011-11-27 13:59:09 +0000

    Klasse für das Saveobjekt, wird als Container für unsere Datenstruktur fungieren

---
+++ JADE/JADE/saveobject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace JADE
+{
+// Wird unsere Datenstruktur enthalten, muss ich aber erst noch definieren...
+
+    [Serializable()]
+    public class SaveObjekt : ISerializable
+    {
+    }
+}
