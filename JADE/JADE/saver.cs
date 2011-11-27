135135ea08a1c766de90df26606e234b79b833dc
robert.studtrucker@gmail.com <robert.studtrucker@gmail.com@43a69330-ffb3-4c6b-9fb1-d670dd4760cb>
2011-11-27 13:58:18 +0000

    Klasse für das Serialisieren der Datenstruktur

---
+++ JADE/JADE/saver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// Klasse welche zum speichern unserer Datenstruktur verwendet wird. Alles sehr allgemein gehalten, zum speichern wird später ein string für den Pfad und ein saveobjekt gebraucht.
+//Für das Saveobjekt git es eine eigene Klasse welche später unsere Datenstruktur erhält und so nur als "Container" fungiert.
+
+namespace JADE
+{
+    class Saver
+    {
+        public Saver()
+        {
+        }
+        //Funktion zum Speichern
+        public void Save(string filename, SaveObjekt saveObjekt)
+        {
+            Stream stream = File.Open(filename, FileMode.Create);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            bFormatter.Serialize(stream, saveObjekt);
+            stream.Close();
+        }
+        //Funktion zum Laden
+        public SaveObjekt DeSave(string filename)
+        {
+            SaveObjekt saveObjekt;
+            Stream stream = File.Open(filename, FileMode.Open);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            saveObjekt = (SaveObjekt)bFormatter.Deserialize(stream);
+            stream.Close();
+            return saveObjekt;
+        }
+    }
+}
