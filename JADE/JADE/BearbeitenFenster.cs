ed3ee2b3f94f8a102d02b68e223697112099c46f
robert.studtrucker@gmail.com <robert.studtrucker@gmail.com@43a69330-ffb3-4c6b-9fb1-d670dd4760cb>
2011-12-19 08:47:06 +0000

    in dem Fenster muss noch eine kleine Anpassung gemacht werden damit das Trennen richtig funktioniert, @Mirco dafuer benoetige ich den Namen des Hauptfensters sowie eine statische variable im Hauptfenster welche standartmäßig unsere Datenstruktur enthält

---
+++ JADE/JADE/BearbeitenFenster.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JADE
+{
+    public partial class BearbeitenFenster : Form
+    {
+        private int Satznummer;
+        private int Tok;
+        private Daten obj;
+
+        public BearbeitenFenster(Daten obj,int Satznummer,int Tok,String str)
+        {
+            this.Satznummer = Satznummer;
+            this.Tok = Tok;
+            this.obj = obj;
+            InitializeComponent();
+            this.textBox3.Text = str;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.Testdaten.trennen2(Satznummer, Tok, this.textBox1.Text, this.textBox2.Text);
+            this.Close();
+        }
+      
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
