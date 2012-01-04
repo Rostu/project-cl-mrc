7e73cd910b0e6fa52f1351665ea2e8d9c129028d
robert.studtrucker@gmail.com <robert.studtrucker@gmail.com@43a69330-ffb3-4c6b-9fb1-d670dd4760cb>
2012-01-04 08:34:58 +0000

    Funktionen zur Rückgabe von einzelnen Sätzen(ArrayList) und Token(String)

--- JADE/JADE/Components/daten.cs
+++ JADE/JADE/Components/daten.cs
@@ -6,10 +6,9 @@ using System.Text;
 using System.Runtime.Serialization;                     
 using System.Runtime.Serialization.Formatters.Binary;
 
-
 // eine extra Klasse für die Arraylist mit unséren Daten also den Sätzen und Token.
 // habe ich extra angelegt weil es dann einfacher ist etwas ab zu aendern wenn wir später vielleicht auch noch Zusatzinformationen wie Uebersetzungen  speichern wollen
-
+//Außerdem enthält die Klasse Funktionen zum trennen und zusammenfuegen von Token, sowie Funktionen zur Rückgabe einzelner Token und Saetze 
 namespace JADE
 {
     [Serializable()]
@@ -71,6 +70,26 @@ namespace JADE
         public Daten(SerializationInfo info, StreamingContext ctxt)
         {
             this.data = (ArrayList)info.GetValue("Daten", typeof(ArrayList));
+        }
+
+        //Funktion zur Rückgabe eines einzelnen Token(String)  
+        public String getToken(int Satznummer, int Tok)
+        {
+            String rueckgabe;
+            ArrayList hilf = new ArrayList();
+            hilf = Form1.Instanzdaten.Zugriff;      
+            ArrayList hilf2 = (ArrayList)hilf[Satznummer];
+            rueckgabe = (String)hilf2[Tok];
+            return rueckgabe;
+        }
+
+        //Funktion zur Rückgabe eines Satzes(ArrayList)
+        public ArrayList getSatz(int Satznummer)
+        {
+            ArrayList hilf = new ArrayList();
+            hilf = Form1.Instanzdaten.Zugriff;
+            ArrayList rueckgabe = (ArrayList)hilf[Satznummer];
+            return rueckgabe;
         }
 
         //Benötigt die Serializer Funktion
