a813d07b0e1695e38436610dae7c25e4ac57bb78
chmichels90@gmail.com <chmichels90@gmail.com@43a69330-ffb3-4c6b-9fb1-d670dd4760cb>
2012-01-26 21:30:08 +0000

    removed naive method information extraction from wadoku.xml; added more sophisticated (yet buggy) solution;

--- JADE/JADE/Components/SearchEngine.cs
+++ JADE/JADE/Components/SearchEngine.cs
@@ -45,14 +45,15 @@ namespace WindowsFormsApplication1
         }
 
         public DataTable search(string token, int satzNr, int tokenNr)
-        {            
-            XElement res =  new XElement("results", 
+        {
+            XElement res = new XElement("results",
                             (from e in wadoku.Root.Elements().OfType<XElement>()
 
-                            where (string) e.Element(rss + "form").Element(rss + "orth").Element(rss + "text") == token
+                             where ((string)e.Element(rss + "form").Element(rss + "orth").Element(rss + "text")).StartsWith(token)
 
-                            select new XElement("result",   e.Element(rss + "form").Element(rss + "pron").Element(rss + "text"),
-                                                            e.Element(rss + "sense").Element(rss + "trans").Element(rss + "tr"))));
+                             select new XElement("result", e.Element(rss + "form").Element(rss + "pron").Element(rss + "text"),
+                                                            new XElement("senses", e.Elements(rss + "sense")))));
+            //Console.Write(res.ToString());
             return createResultTable(res, token, satzNr, tokenNr);
         }
 
@@ -67,7 +68,7 @@ namespace WindowsFormsApplication1
         {
             DataTable res;
             // Create a new DataTable as the result of the search.
-            DataTable result = new DataTable("Suchergebnisse - " + token + " (Satz " + satzNr + ", Token " + tokenNr + ")");
+            DataTable result = new DataTable("results_" + token + "_" + satzNr + "_" + tokenNr);
             // Declare variables for DataColumn and DataRow objects.
             DataColumn column;
             DataRow row;
@@ -76,8 +77,8 @@ namespace WindowsFormsApplication1
             // Create new DataColumn, set DataType, 
             // ColumnName and add to DataTable.    
             column = new DataColumn
-            { 
-                DataType = System.Type.GetType("System.Int32"), 
+            {
+                DataType = System.Type.GetType("System.Int32"),
                 ColumnName = "Eintrag",
                 ReadOnly = true,
             };
@@ -132,13 +133,143 @@ namespace WindowsFormsApplication1
             // Create three new DataRow objects and add 
             // them to the DataTable
 
-            int count = 0;
+            int resultCount = 0;
 
-            foreach (XElement r in resEntries.Nodes())
-            {   
-                //ArrayList tokens = new ArrayList((ICollection) );
-                //MessageBox.Show("" + tokens.Count);
-                result.Rows.Add(++count, r.Element(rss + "text").Value, String.Join(", ", ((from t in r.Element(rss + "tr").Elements() select ((string)t)).ToArray())));
+            foreach (XElement resultEntry in resEntries.Nodes())
+            {
+                List<String> senses = new List<String>();
+
+                foreach (XElement sense in resultEntry.Element("senses").Elements(rss + "sense"))
+                {
+                    //Console.WriteLine(sense.ToString());
+                    List<String> transPerSense = new List<String>();
+                    string senseData = "";
+
+                    #region Einzelne_<trans>'s_auflisten
+                    foreach (XElement trans in sense.Elements(rss + "trans"))
+                    {
+                        //Console.WriteLine(trans.ToString());
+                        List<String> trPerTrans = new List<String>();
+                        string transData = "";
+
+                        #region Einzelne_<tr>'s_auflisten
+                        foreach (XElement tr in trans.Elements(rss + "tr"))
+                        {
+                            //Console.WriteLine(tr.ToString());
+                            bool first = true;
+                            bool followingSpaceOld = false;
+                            bool followingSpaceNew = false;
+                            bool preceedingSpace = false;
+
+                            List<String> elemPerTr = new List<String>();
+                            string trData = "";
+
+                            #region Elemente_fuer_tr_konkatenieren
+
+                            foreach (XElement elem in tr.Elements())
+                            {
+                                //Console.WriteLine(elem.ToString());
+                                string elemData = "";
+
+                                if (elem.Name == rss + "text")
+                                {
+                                    if (elem.Attribute("hasPrecedingSpace") != null)
+                                    {
+                                        if (elem.Attribute("hasPrecedingSpace").Value == "true")
+                                        {
+                                            preceedingSpace = true;
+                                        }
+                                    }
+                                    if (elem.Attribute("hasFollowingSpace") != null)
+                                    {
+                                        if (elem.Attribute("hasFollowingSpace").Value == "true")
+                                        {
+                                            followingSpaceNew = true;
+                                        }
+                                    }
+                                    elemData = elem.Value;
+                                    //a
+                                }
+                                else if (elem.Name == rss + "token")
+                                {
+                                    string tokenInfo = "";
+
+                                    if (elem.Attribute("type") != null)
+                                    {
+                                        tokenInfo = tokenInfo + " [";
+
+                                        if (elem.Attribute("type").Value == "N")
+                                        {
+                                            string numerus = "";
+                                            string genus = "";
+
+                                            foreach (XAttribute attr in elem.Attributes())
+                                            {
+                                                switch (attr.Name.ToString())
+                                                {
+                                                    case "genus":
+                                                        genus = elem.Attribute("genus").Value; break;
+                                                    case "numerus":
+                                                        numerus = (elem.Attribute("numerus").Value == "pl") ? "Pl." : "Sg."; break;
+                                                }
+                                            }
+                                            if (genus != "") tokenInfo = tokenInfo + genus + ".";
+                                            if (numerus != "") tokenInfo = tokenInfo + ", " + numerus;
+                                            tokenInfo = tokenInfo + "]";
+                                        }
+                                    }
+                                    elemData = elem.Value + tokenInfo;
+                                    //a
+                                }
+                                Console.WriteLine(elemData);
+                                if (!(elemData == ""))
+                                {
+                                    if (first)
+                                    {
+                                        trData = trData + elemData + ((followingSpaceNew) ? " " : "");
+                                        first = false;
+                                    }
+                                    else
+                                    {
+                                        if (!followingSpaceOld)         // Falls das vorherige Element kein Leerzeichen am Ende aufweist, ...
+                                        {
+                                            if (!preceedingSpace)           // ... und falls das gerade betrachtete Element auch kein führendes Leerzeichen erhalten soll, ...
+                                            {
+                                                trData = trData + ((elemData.StartsWith(", ")) ? "" : ", ");        // ... so füge, falls nicht bereits vorhanden, ", " als Trennelement ein.
+                                            }
+                                            else                            // ... und falls das gerade betrachtete Element ein führendes Leerzeichen erhalten soll, ...
+                                            {
+                                                trData = trData + " ";                                              // ... so füge das führende Leerzeichen ein.
+                                            }
+                                        }
+                                        trData = trData + elemData + ((followingSpaceNew) ? " " : "");
+                                    }
+
+                                    followingSpaceOld = followingSpaceNew;
+                                    followingSpaceNew = false;
+                                    preceedingSpace = false;
+                                }
+                            }
+                            #endregion
+
+                            trPerTrans.Add(trData);
+                        }
+                        #endregion
+
+                        transData = String.Join(", ", trPerTrans.ToArray());
+                        transPerSense.Add(transData);
+
+                    } 
+                    #endregion
+
+                    senseData = String.Join("; ", transPerSense.ToArray());
+                    senses.Add(senseData);
+                    //translations.Add(senseData + String.Join(", ", translationsPerSense.ToArray()));
+                }
+               
+                int senseCount = 1;
+
+                result.Rows.Add(++resultCount, resultEntry.Element(rss + "text").Value, "(1) " + String.Join((". (" + ++senseCount + ") "), senses) + ".");
             }
             res = result;
             return res;
